d88dc11aa3516898eddcc4e561ef96696b5c7d1f
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-07-10 13:57:06 +0530

    Add to Cart Fixes

--- expense/shoppingAppReducers/src/components/Shop/Products.js
+++ expense/shoppingAppReducers/src/components/Shop/Products.js
@@ -1,16 +1,27 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+  {id: 'p1', price: 6, title: 'My First Book', description: 'the first book i ever wrote' },
+  {id: 'p2', price: 7, title: 'My Second Book', description: 'its amaizing experience' },
+  {id: 'p3', price: 4, title: 'My Third Book', description: 'i recently wrote this' },
+]
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product) => (
+           <ProductItem
+           title={product.title}
+           price={product.price}
+           description={product.description}
+           key={product.id}
+           id={product.id}
+         />
+        ))}
+       
       </ul>
     </section>
   );
