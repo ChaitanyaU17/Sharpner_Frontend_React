bd699f1865891400daed9170204d21d2676b7aeb
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-07-11 16:50:03 +0530

    test case

--- expense/src/components/ExpenstList.test.js
+++ expense/src/components/ExpenstList.test.js
@@ -1,5 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import ExpenseTable from "./ExpenseList";
+
+//test cases
+
 describe("Expense Component", () => { //for grouping the test
   test("render Expense List as a Text", () => {
     render(<ExpenseTable />);
