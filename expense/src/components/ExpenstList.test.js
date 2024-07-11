dfca6e73485a52c2a05cbae000120b00b45df6bd
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-07-11 18:52:38 +0530

    testing user Action

--- expense/src/components/ExpenstList.test.js
+++ expense/src/components/ExpenstList.test.js
@@ -3,10 +3,24 @@ import ExpenseTable from "./ExpenseList";
 
 //test cases
 
-describe("Expense Component", () => { //for grouping the test
-  test("render Expense List as a Text", () => {
+// describe("Expense Component", () => { //for grouping the test
+//   test("render Expense List as a Text", () => {
+//     render(<ExpenseTable />);
+//     const ExpenseList = screen.getByText("Expense List", {exact: false});
+//     expect(ExpenseList).toBeInTheDocument();
+//   });
+// });
+
+describe('Expense List', () => {
+  test('render ZERO EXPENSE FOUND as a text', () => {
+
+    //Arrange
     render(<ExpenseTable />);
-    const ExpenseList = screen.getByText("Expense List");
-    expect(ExpenseList).toBeInTheDocument();
+
+    //Act
+    //...nothing
+    //Assert
+    const outputText = screen.getByText('ZERO EXPENSE FOUND');
+    expect(outputText).toBeInTheDocument();
   });
 });
