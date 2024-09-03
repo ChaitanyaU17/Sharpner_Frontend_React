e5b107ac8619b5516fb56ff178e3dc1abd9e64b2
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-09-03 14:51:41 +0530

    signup page implemented

--- MailBoxClient/mail-box-client/src/App.js
+++ MailBoxClient/mail-box-client/src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
+import React from 'react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Signup from './components/Signup';
+import Login from './components/Login';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/signup',
+      element: <Signup />
+    },
+    {
+      path: '/login',
+      element: <Login />
+    },
+    {
+      path: '/',
+      element: <Signup /> // Set a default route if needed
+    }
+  ]);
+
   return (
-    <div className="App">
-      Hello
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
---
+++ MailBoxClient/mail-box-client/src/components/Signup.js
@@ -0,0 +1,108 @@
+import React, { useContext, useRef } from 'react';
+import { Link } from 'react-router-dom';
+import AuthContext from '../store/AuthContext';
+
+const Signup = () => {
+    const emailRef = useRef();
+  const passwordRef = useRef();
+  const confirmPasswordRef = useRef();
+
+  const authctx = useContext(AuthContext);
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    const confirmPassword = confirmPasswordRef.current.value;
+
+    if (password !== confirmPassword) {
+      alert("Passwords don't match");
+      passwordRef.current.value = "";
+      confirmPasswordRef.current.value = "";
+    } else {
+      const res = await fetch(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.REACT_APP_KEY}`,
+        {
+          method: "POST",
+          body: JSON.stringify({
+            email: email,
+            password: password,
+            returnSecureToken: true,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await res.json();
+      if (res.ok) {
+        authctx.addIdToken(data.idToken);
+        authctx.addEmail(email);
+      } else {
+        let errMessage = "Authentication Failed...";
+        if (data && data.error && data.error.message) {
+          errMessage = data.error.message;
+        }
+        alert(errMessage);
+      }
+    }
+
+    emailRef.current.value = "";
+    passwordRef.current.value = "";
+    confirmPasswordRef.current.value = "";
+  };
+
+  return (
+    <div className='container'>
+      <div className='row justify-content-center border'>
+        <div className='col-md-6 border border-black p-5 mt-3 rounded-5'>
+          <h2 className='text-center mb-4'>Signup</h2>
+          <form onClick={submitHandler}>
+            <div className='form-group mb-3'>
+              <label htmlFor='email'>Email address</label>
+              <input
+                type='email'
+                className='form-control'
+                id='email'
+                ref={emailRef}
+                placeholder='Enter email'
+                required
+              />
+            </div>
+            <div className='form-group mb-3'>
+              <label htmlFor='password'>Password</label>
+              <input
+                type='password'
+                className='form-control'
+                id='password'
+                ref={passwordRef}
+                placeholder='Password'
+                required
+              />
+            </div>
+            <div className='form-group mb-3'>
+              <label htmlFor='confirmPassword'>Confirm Password</label>
+              <input
+                type='password'
+                className='form-control'
+                id='confirmPassword'
+                ref={confirmPasswordRef}
+                placeholder='Confirm Password'
+                required
+              />
+            </div>
+            <button type='submit' className='btn btn-primary btn-block'>
+              Signup
+            </button>
+          </form>
+          <div className='text-center mt-3'>
+            <p>Already a user? <Link to='/login' style={{ color: 'blue' }}>Login</Link></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Signup;
