aa28fd6e5b02ea1340057c7f552bb612a7d2f88b
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-09-03 19:00:28 +0530

    login page test cases implemented Ensures the form does not submit with empty fields

---
+++ MailBoxClient/mail-box-client/src/components/Login.test.js
@@ -0,0 +1,12 @@
+import { screen, render, fireEvent } from '@testing-library/react';
+import Login from './Login';
+
+test('prevents form submission when fields are empty', () => {
+    render(<Login/>);
+  
+    fireEvent.click(screen.getByRole('button', { name: /LogIn/i }));
+  
+    expect(screen.getByLabelText(/Email/i).value).toBe('');
+    expect(screen.getByLabelText(/Password/i).value).toBe('');
+  });
+  
