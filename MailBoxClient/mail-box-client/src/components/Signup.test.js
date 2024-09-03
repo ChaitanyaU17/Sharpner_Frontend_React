d4c19bf13e11c5217a597a2c0e9f9462ce032161
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-09-03 16:52:22 +0530

    signup test cases implement

---
+++ MailBoxClient/mail-box-client/src/components/Signup.test.js
@@ -0,0 +1,10 @@
+import Signup from "./Signup";
+import { render, screen } from '@testing-library/react';
+
+test('renders Signup component with email, password, and confirm password fields', () => {
+    render(<Signup />);
+    
+    expect(screen.getByLabelText(/Email address/i)).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /Signup/i })).toBeInTheDocument();
+  });
+  
