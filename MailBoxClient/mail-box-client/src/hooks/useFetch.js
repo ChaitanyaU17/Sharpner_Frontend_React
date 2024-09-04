6e284fe93c32fc369c1ef4a0eef7e41c4328c763
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-09-04 19:56:17 +0530

    fetch mail data

---
+++ MailBoxClient/mail-box-client/src/hooks/useFetch.js
@@ -0,0 +1,13 @@
+const fetchMail = async (email, pre) => {
+    const res = await fetch(`${process.env.REACT_APP_DATABASE_URL}/${pre}${email}.json`);
+
+    let data = await res.json();
+    try {
+        data = Array.from(Object.entries(data));
+    } catch (error) {
+        data = [];
+    }
+    return data;
+}
+
+export default fetchMail;
