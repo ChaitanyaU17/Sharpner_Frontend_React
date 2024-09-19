827982785745d20ea292bdf19fd72ef125107653
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-09-19 14:56:13 +0530

    add routes to category recipe and order pages

--- Three days test project/Admin Panel/src/App.jsx
+++ Three days test project/Admin Panel/src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import Signup from "./auth/Signup";
 import Login from "./auth/Login";
 import Home from "./components/Home";
+import CategoriesManagement from "./components/CategoriesManagement"; 
+import RecipesManagement from "./components/RecipesManagement";
+import OrdersManagement from "./components/OrdersManagement";
 
 const App = () => {
   return (
@@ -11,6 +14,9 @@ const App = () => {
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/manage-categories" element={<CategoriesManagement />} />
+        <Route path="/manage-recipes" element={<RecipesManagement />} />
+        <Route path="/manage-orders" element={<OrdersManagement />} />
       </Routes>
     </Router>
   );
--- Three days test project/Admin Panel/src/components/Home.jsx
+++ Three days test project/Admin Panel/src/components/Home.jsx
@@ -1,10 +1,47 @@
+import { Link, useNavigate } from "react-router-dom";
+
 const Home = () => {
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    alert('Logged out successfully!');
+    navigate('/login');
+  }
+
     return (
-      <div className="flex items-center justify-center h-screen">
-        <h1 className="text-4xl font-bold">Welcome to Home!</h1>
+    <div className="p-6 ">
+      <div className="flex items-center justify-between mb-4">
+      <h1 className="text-3xl font-bold mb-6">Admin Dashboard</h1>
+      <button onClick={handleLogout} className="bg-red-500 p-2 rounded-lg">Logout</button>
       </div>
-    );
-  };
-  
-  export default Home;
+      
+      <nav>
+        <ul className="flex items-center gap-x-4 text-xl font-semibold">
+          <li className="mb-4 bg-black p-2 rounded-md h-32 w-48 pt-10 flex justify-center">
+            <Link to="/manage-categories" className="text-blue-500 hover:underline">
+              Manage Categories
+            </Link>
+          </li>
+          <li className="mb-4 bg-black p-2 rounded-md h-32 w-44 pt-10 flex justify-center">
+            <Link to="/manage-recipes" className="text-blue-500 hover:underline">
+              Manage Recipes
+            </Link>
+          </li>
+          <li className="mb-4 bg-black p-2 rounded-md h-32 w-44 pt-10 flex justify-center">
+            <Link to="/manage-orders" className="text-blue-500 hover:underline">
+              Manage Orders
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      
+    </div>
+  );
+};
+
+export default Home;
+
   
