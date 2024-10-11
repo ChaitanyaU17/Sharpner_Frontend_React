70a3636f5c49b079f791099a1af6776ec6b5a886
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-10-11 22:39:26 +0530

    configure routes

--- Three days test project/User Interface/src/App.jsx
+++ Three days test project/User Interface/src/App.jsx
@@ -1,13 +1,74 @@
-import './App.css'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import Login from "./auth/Login";
+import Signup from "./auth/Signup";
+import Home from "./components/Home";
+import Categories from "./components/Categories";
+import CategoryRecipes from "./components/CategoryRecipes";
+import UserProfile from "./components/UserProfile";
+import Cart from "./components/Cart";
+import Checkout from "./components/Checkout";
+import OrderStatus from "./components/OrderStatus";
 
 function App() {
+  const [userId, setUserId] = useState(null);
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const storedUserId = localStorage.getItem("userId");
+    const storedToken = localStorage.getItem("token");
+    setUserId(storedUserId);
+    setToken(storedToken);
+  }, []);
+
   return (
-    <>
-     <div className='text-red-400'>
-      Hello user
-     </div>
-    </>
-  )
+    <Router>
+      <Routes>
+        {/* Authentication Routes */}
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+
+        {/* Home Route */}
+        <Route path="/home" element={<Home />} />
+
+        {/* Browse Categories */}
+        <Route path="/categories/" element={<Categories />} />
+
+        {/* List recipes under a specific category */}
+        <Route
+          path="/categories/:categoryName"
+          element={<CategoryRecipes userId={userId} token={token} />}
+        />
+
+        {/* User Profile */}
+        <Route
+          path="/profile"
+          element={<UserProfile userId={userId} token={token} />}
+        />
+
+        {/* Cart */}
+        <Route path="/cart" element={<Cart userId={userId} token={token} />} />
+
+        {/* Checkout */}
+        <Route
+          path="/checkout"
+          element={<Checkout userId={userId} token={token} />}
+        />
+
+        {/* Order Status */}
+        <Route
+          path="/orders"
+          element={<OrderStatus userId={userId} token={token} />}
+        />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
